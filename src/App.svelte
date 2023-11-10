e7ee312e140c9179f462d2b0b3b7e8eaabe3f4e9
Manuel A. Sanchez Villa <manurobot.20@gmail.com>
2023-11-09 19:18:22 -0600

    "Modifique el scripts de FETCH"

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,13 @@
 <script>
-	export let name;
+	import { onMount } from 'svelte'
+	let data
+	onMount(async () => {
+		data = await fetch('https://desormex.com/JuegosLSM_Back/public/api/desormex/juego-memoria').then(x => x.json())
+	})
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<h1>Posts</h1>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<pre>
+	{JSON.stringify(data, null, 2)}
+</pre>

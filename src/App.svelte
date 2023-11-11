e9e9b902d1719bd93f8460a87de4692ba23081c4
Manuel A. Sanchez Villa <manurobot.20@gmail.com>
2023-11-10 22:21:39 -0600

    "10 Nov 2023"

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,13 @@
 <script>
 	import axios from 'axios';
-	import baseUrl from '../env/environments'
+	// import baseUrl from '../env/environments'
 
-	// export let url = 'https://desormex.com/JuegosLSM_Back/public/api/desormex/juego-memoria';
+	export let url = 'https://desormex.com/JuegosLSM_Back/public/api/desormex/juego-memoria';
 
 	let List = [];
 
 	const getPosts = async () => {
-  const response = await axios.get(baseUrl);
+  const response = await axios.get(url);
   if (response.status === 200) {
     const posts = response.data.Payload;
 	List = posts;
@@ -23,7 +23,18 @@ getPosts();
 
 <ul>
   {#each List as post}
-    <li>{post.Nombre}</li>
-	<img src="{post.Base}" width="100px"/>
+    <li>{post.Nombre}
+      <!-- {src} is short for src={src} -->
+	  <img src="{post.Base}" width="100px">
+    </li>
   {/each}
 </ul>
+
+
+<style>
+	ul>li {
+		color: crimson;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
